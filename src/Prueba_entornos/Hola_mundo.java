681a178a48aecf1be63bb5e18cacde61baa7cdca
lizbercas <lizbercas@client>
2023-01-25 12:11:53 +0100

    este es mi primer comit

---
+++ src/Prueba_entornos/Hola_mundo.java
@@ -0,0 +1,9 @@
+package Prueba_entornos;
+
+public class Hola_mundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola, toy cansada jorge!!!!!");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author lizbercas
+ *
+ */
+module entornos {
+}
